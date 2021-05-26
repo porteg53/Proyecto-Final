8745542cd5e3d6da9bc33676dab979b8e70ffbe0
Gabriela-Serrano-Lopez <75129974+Gabriela-Serrano-Lopez@users.noreply.github.com>
2021-05-25 19:40:51 -0500

    main

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { PlatilloService } from './shared/platillo.service';
 import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +23,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+ 
   ],
   providers: [
     PlatilloService
