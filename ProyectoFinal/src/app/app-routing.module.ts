495623afb534a1f9d5df73210d87dd2d4d73aa13
Gabriela-Serrano-Lopez <75129974+Gabriela-Serrano-Lopez@users.noreply.github.com>
2021-06-03 17:13:09 -0500

    main

--- ProyectoFinal/src/app/app-routing.module.ts
+++ ProyectoFinal/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactoComponent} from './contacto/contacto.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent} from './login/login.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {NosotrosComponent} from './nosotros/nosotros.component';
 import {AdmComponent} from './adm/adm.component';
 const routes: Routes = [
-  {path: 'contacto', component: ContactoComponent},
+
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'navbar',component:NavbarComponent},
