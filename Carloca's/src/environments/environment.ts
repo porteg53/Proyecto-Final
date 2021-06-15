b7b59a006c64718589aff5e1120c132d6b11cf17
porteg53 <81374584+porteg53@users.noreply.github.com>
2021-06-15 18:09:22 -0500

    Update environment.ts
    configuración firebase
    
    Co-Authored-By: porteg53 <81374584+porteg53@users.noreply.github.com>

--- Carloca's/src/environments/environment.ts
+++ Carloca's/src/environments/environment.ts
@@ -5,14 +5,13 @@
 export const environment = {
 production: false,
 firebaseConfig: {
-  apiKey: "AIzaSyCp3Y5bacwvlWRTZv7tl0U2JNlHl46_iZU",
-  authDomain: "gina-a4be0.firebaseapp.com",
-  databaseURL: "https://gina-a4be0.firebaseio.com",
-  projectId: "gina-a4be0",
-  storageBucket: "gina-a4be0.appspot.com",
-  messagingSenderId: "604220098681",
-  appId: "1:604220098681:web:2b8bfde7e8268bac682ca8",
-  measurementId: "G-0BFTHL94C1"
+  apiKey: "AIzaSyDrsc9rbiqMmgzM1nXPs2a9tGg-bLGOwIE",
+  authDomain: "carloca-e022c.firebaseapp.com",
+  projectId: "carloca-e022c",
+  storageBucket: "carloca-e022c.appspot.com",
+  messagingSenderId: "140909315752",
+  appId: "1:140909315752:web:bdc2884cb205d8bc4c249d"
+  //measurementId: "G-0BFTHL94C1"
 }
 };
 
